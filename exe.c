761b49b1255459e7bf5d0ddb30d7bfbfa1ca3358
machenfeng <495660712@qq.com>
2024-12-09 17:42:23 +0800

    some exe

---
+++ exe.c
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2024 LingYun<iot25@lingyun>
+ *                  All rights reserved.
+ *
+ *       Filename:  exe.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(09/12/24)
+ *         Author:  LingYun <iot25@lingyun>
+ *      ChangeLog:  1, Release initial version on "09/12/24 17:40:47"
+ *                 
+ ********************************************************************************/
+
+"hello github"
